2276884ac88738a7da7e5ceb06c12f28375f4d3f
Jarrad Whitaker <akdor1154@gmail.com>
2017-12-03 21:39:52 +1100

    makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+default:
+	./make.sh
---
+++ make.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VERSION=$(sed -n -E -e 's/"version": "(.+)",/\1/p' manifest.json | tr -d '[:space:]')
+ID=$(sed -n -E -e 's/"id": "(.+)"/\1/p' manifest.json | tr -d '[:space:]')
+
+ZIPNAME=${ID}-${VERSION}.xpi
+
+echo "${ZIPNAME}"
+
+zip "${ZIPNAME}" fitToWidth.js manifest.json
